e49a38b8ffa8145c74f6a0577b066d43fa35386a
tim <tim@crunchbookpro>
2014-05-24 14:47:47 -0700

    Removed node env stuff

--- db/db.js
+++ db/db.js
@@ -1,11 +1,8 @@
 var influx = require("influx"),
     winston = require("winston");
 
-var env = process.env.NODE_ENV || "test";
 var db;
 
-if (env == "test") {
-	db = influx("localhost", 8086, "tmlbl", "tmlbl", "forex");
-}
+db = influx("localhost", 8086, "root", "root", "forex");
 
 module.exports.db = db;
