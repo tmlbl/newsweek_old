ae5909aabe122dd13a834e688a09795ddeba99d7
tmlbl <tim@shippable.com>
2014-08-31 09:36:24 -0700

    Trader test

--- modules/trader/trader.js
+++ modules/trader/trader.js
@@ -16,19 +16,19 @@ function Trader(options, trade) {
     if (err) {
       logger.error(err);
     }
-    logger.debug('Created trade group', self.model.toObject());
+    logger.debug('Created trade group', self.model.id);
     self.schedule();
   });
 }
 
 Trader.prototype.schedule = function () {
   var t = this.trade.time;
-  logger.debug('Scheduling trade at', t);
+  logger.debug('Scheduling trade', this.model.id, 'at', t);
   s.scheduleJob(t, this.execute.bind(null, this));
 };
 
 Trader.prototype.execute = function (self) {
-  logger.debug('Executing the trade');
+  logger.debug('Executing trade', self.model.id);
   self.client.openTrade(self.trade, function (err, res) {
     logger.debug('Opened', res);
   });
--- test/unit/trader_test.js
+++ test/unit/trader_test.js
@@ -4,10 +4,10 @@ var Trader = require('../../modules/trader/trader'),
 require('../../common/logger');
 
 describe('Trader class', function () {
-  var trader;
+  var trade;
 
   it('should initialize the class', function () {
-    trader = new Trader({
+    trade = new Trader({
       account: 	8564825,
       token: '7d5100fcb48f09bff2c85a92b5ef6639-' +
           '744dfed42981e7020c732a34184da930'
@@ -18,14 +18,17 @@ describe('Trader class', function () {
       instrument: 'EUR_USD',
       event: mongoose.Types.ObjectId()
     });
-    if (!trader.execute) {
+    if (!trade.execute) {
       throw new Error('Didn\'t find the class object');
+    }
+    if (trade.completed) {
+      throw new Error('Trade was initialized as already completed');
     }
   });
 
   it('should be marked as completed', function (next) {
     setTimeout(function () {
-      if (!trader.completed) {
+      if (!trade.completed) {
         throw new Error('The trade was not marked as completed');
       }
     }, 900);
